b4ca82c6dd3c35ec054dbe08a781ad2fcd724e57
Shubham Patel <shubhampatel1573@gmail.com>
2026-01-08 13:45:18 +0530

    2d array completed

---
+++ 2dArray/searchMatrix.java
@@ -0,0 +1,28 @@
+public class searchMatrix {
+    public static boolean staircaseSearch(int matrix[][], int key){
+        int row = 0, col= matrix.length-1;
+
+        while (row<matrix.length && col>=0) {
+            if (matrix[row][col] == key) {
+                System.out.print("found key at (" + row + "," + col + ")");
+                return true;
+            } else if (key< matrix[row][col]) {
+                col--;
+            } else{
+                row++;
+            }
+        }
+        System.out.println("key not found1");
+        return false;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {10,20,30,40},
+            {15,25,35,45},
+            {27,29,37,48},
+            {32,33,39,50}
+        };
+        int key = 33;
+        staircaseSearch(matrix, key);
+    }
+}
